7181b70d767e439555b202b2b4e7151bd2827790
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-07-29 13:37:15 +0000

    Task 2: Add type annotations and math.floor usage

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for annotated floor function."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float number as an integer."""
+    return math.floor(n)
