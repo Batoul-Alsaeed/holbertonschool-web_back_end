402238a7495007783a3a55e4baa8b313fb9d2412
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-07-31 14:21:43 +0000

    Fix Task 3: Add module docstring for task_wait_random function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Module that defines task_wait_random function.
+This function returns an asyncio.Task wrapping wait_random coroutine.
+"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
