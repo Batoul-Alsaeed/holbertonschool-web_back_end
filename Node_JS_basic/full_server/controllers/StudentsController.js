b8073d0a6aca7a762724ce8503bb1546909525b2
Batoul-Alsaeed <10672@holbertonstudents.com>
2025-09-07 19:10:11 +0000

    Fix Task 8: plain-text responses, alphabetical field order, and runtime DB path in controllers

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,41 +1,49 @@
 import readDatabase from '../utils';
 
-export default class StudentsController {
-  static async getAllStudents(_req, res) {
-    const dbPath = process.argv[2]; // read DB path at execution time
-    try {
-      const groups = await readDatabase(dbPath);
+class StudentsController {
+  static getAllStudents(request, response, DATABASE) {
+    readDatabase(DATABASE)
+      .then((fields) => {
+        const students = [];
+        // let count = 0;
+        let msg;
 
-      const lines = ['This is the list of our students'];
-      const fields = Object.keys(groups).sort((a, b) =>
-        a.localeCompare(b, undefined, { sensitivity: 'base' })
-      );
+        // for (const key of Object.keys(fields)) {
+        //   count += fields[key].length;
+        // }
 
-      for (const field of fields) {
-        const list = groups[field] || [];
-        lines.push(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-      }
+        // students.push(`Number of students: ${count}`);
+        students.push('This is the list of our students');
 
-      res.status(200).type('text').send(lines.join('\n'));
-    } catch (_e) {
-      res.status(500).type('text').send('Cannot load the database');
-    }
+        for (const key of Object.keys(fields)) {
+          msg = `Number of students in ${key}: ${
+            fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          students.push(msg);
+        }
+        response.send(200, `${students.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+      });
   }
 
-  static async getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
-    if (major !== 'CS' && major !== 'SWE') {
-      res.status(500).type('text').send('Major parameter must be CS or SWE');
-      return;
-    }
+  static getAllStudentsByMajor(request, response, DATABASE) {
+    const { major } = request.params;
 
-    const dbPath = process.argv[2];
-    try {
-      const groups = await readDatabase(dbPath);
-      const list = groups[major] || [];
-      res.status(200).type('text').send(`List: ${list.join(', ')}`);
-    } catch (_e) {
-      res.status(500).type('text').send('Cannot load the database');
+    if (major !== 'CS' && major !== 'SWE') {
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(DATABASE)
+        .then((fields) => {
+          const students = fields[major];
+
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => response.send(500, 'Cannot load the database'));
     }
   }
 }
+
+export default StudentsController;
